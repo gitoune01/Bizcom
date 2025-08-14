4946309d3cfde0f2ead67e2d2da1be545e7d908d
gitoune01 <docgyno@gmail.com>
2025-08-14 18:28:53 +0200

    yzez

--- components/shared/header/user-btn.tsx
+++ components/shared/header/user-btn.tsx
@@ -1,12 +1,29 @@
-const UserBtn = () => {
-  return (  
-    <>
-    
-    user
-    </>
+import Link from 'next/link';
+import { auth } from '@/auth';
+import { signOutUser } from '@/lib/actions/user.actions';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+} from '@/components/ui/dropdown-menu';
+import { User2Icon } from 'lucide-react';
 
+const UserBtn = async () => {
+  const session = await auth();
+  console.log('session', session);
+  if (!session) {
+    return (
+      <Button asChild>
+        <Link href="/sign-in">
+          <User2Icon /> Sign In
+        </Link>
+      </Button>
+    );
+  }
+  return <>user</>;
+};
 
-  );
-}
- 
 export default UserBtn;
