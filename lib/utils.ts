1373f77cbfbe63c59904f03aff947c807e78bb84
gitoune01 <docgyno@gmail.com>
2025-08-17 10:03:18 +0200

    dsds

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,7 +9,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export function convertToJsObject<T>(value: T): T {
   0;
- 
+
   return JSON.parse(JSON.stringify(value));
 }
 
@@ -24,18 +24,49 @@ export function formatNumberWithDecimal(num: number): string {
 //form fiels errors handler
 
 //eslint-disable-next-line @typescript-eslint/no-explicit-any
-export async function formatError(error:any) {
-
+export async function formatError(error: any) {
   //handle zod errors
- if(error === "ZodError") {
-    const errMsg = JSON.parse(error).map((error: any) => error.message).join(', ');
-    console.log("error))))))))))))))))>", errMsg);
-    return errMsg
+  if (error === 'ZodError') {
+    const errMsg = JSON.parse(error)
+      .map((error: any) => error.message)
+      .join(', ');
 
-  }else if(error.name === "PrismaClientKnownRequestError" && error.code === "P2002") {
+    return errMsg;
+  } else if (
+    error.name === 'PrismaClientKnownRequestError' &&
+    error.code === 'P2002'
+  ) {
     //handle prisma errors
-    
-  }else {
+  } else {
     //handle other errors
+  }
+}
+
+//round number to two decimal places
+export function roundToTwo(value: number | string) {
+  if (typeof value === 'number') {
+    return Math.round((value + Number.EPSILON) * 100) / 100;
+  } else if (typeof value === 'string') {
+    return Math.round((Number(value) + Number.EPSILON) * 100) / 100;
+  } else {
+    throw new Error('value must be a number or string');
+  }
+}
+
+//convert into any international currency format
+const CURRENCY_FORMATTER = new Intl.NumberFormat('en-US', {
+  currency: 'USD',
+  style: 'currency',
+  minimumFractionDigits: 2,
+});
+
+//format currency using the formatter above
+export function formatCurrency(amount: number | string | null) {
+  if (typeof amount === 'number') {
+    return CURRENCY_FORMATTER.format(amount);
+  } else if (typeof amount === 'string') {
+    return CURRENCY_FORMATTER.format(Number(amount));
+  } else {
+    return 'NaN';
   }
 }
